61b1b8b12229275b3c66f6407c50f05e2c6037f3
nrinaudo <nicolas@nrinaudo.com>
2017-02-07 16:21:02 +0100

    Move version numbers to a separate file.

---
+++ project/Versions.scala
@@ -0,0 +1,13 @@
+object Versions {
+  val sbtHeaders: String  = "1.6.0"
+  val sbtSonatype: String = "1.1"
+  val sbtPgp: String      = "1.0.0"
+  val tut: String         = "0.4.8"
+  val sbtSite: String     = "1.1.0"
+  val sbtUnidoc: String   = "0.3.3"
+  val sbtGhPages: String  = "0.6.0"
+  val sbtHeader: String   = "1.6.0"
+  val scalastyle: String  = "0.8.0"
+  val scoverage: String   = "1.5.0"
+  val sbtDoctest: String  = "0.5.0"
+}
