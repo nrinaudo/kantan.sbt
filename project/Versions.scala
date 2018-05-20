b3bcf4c44817e898ea86da12c881b709b2ec37fa
Nicolas Rinaudo <nicolas@nrinaudo.com>
2018-05-20 13:36:45 +0200

    [#80] Update to sbt-scalafmt 1.5.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val sbtHeader: String   = "5.0.0"
   val sbtPgp: String      = "1.1.1"
   val sbtRelease: String  = "1.0.8"
-  val sbtScalafmt: String = "1.4.0"
+  val sbtScalafmt: String = "1.5.1"
   val sbtSite: String     = "1.3.2"
   val sbtSonatype: String = "2.0"
   val sbtUnidoc: String   = "0.4.1"
