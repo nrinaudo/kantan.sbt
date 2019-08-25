609a20c243630330f9aaded1485b4d1d1bee681c
Nicolas Rinaudo <nicolas@nrinaudo.com>
2019-08-25 13:29:33 +0200

    [#88] Update to scalajs cross 0.6.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -12,7 +12,7 @@ object Versions {
   val sbtSonatype: String  = "3.8"
   val sbtUnidoc: String    = "0.4.2"
   val scalajs: String      = "0.6.28"
-  val scalajsCross: String = "0.6.0"
+  val scalajsCross: String = "0.6.1"
   val scalastyle: String   = "1.0.0"
   val scoverage: String    = "1.6.0"
   val wartRemover: String  = "2.4.2"
