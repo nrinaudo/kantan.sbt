bf5db883a6355dbe9f740bf4bcc29b37cfc4646a
exoego <ytatsuno.jp@gmail.com>
2020-05-21 17:54:33 +0900

    Drop Scala.js 0.6 support to avoid cost of cross-building

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val sbtSite: String      = "1.4.0"
   val sbtSonatype: String  = "3.9.2"
   val sbtUnidoc: String    = "0.4.3"
-  val scalajs: String      = Option(System.getenv("SCALAJS_VERSION")).filter(_.nonEmpty).getOrElse("1.1.0")
+  val scalajs: String      = "1.1.0"
   val scalajsCross: String = "0.6.1"
   val scalastyle: String   = "1.0.0"
   val scoverage: String    = "1.6.1"
