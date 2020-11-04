c64adfd111944b4703fd469ffcfc1776f6981776
Scala Steward <me@scala-steward.org>
2020-11-04 16:49:17 +0100

    Update sbt-mdoc to 2.2.11

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val boilerplate: String  = "0.6.1"
-  val mdoc: String         = "2.2.10"
+  val mdoc: String         = "2.2.11"
   val sbtDoctest: String   = "0.9.7"
   val sbtGhPages: String   = "0.6.3"
   val sbtHeader: String    = "5.6.0"
