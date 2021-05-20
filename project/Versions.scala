6d264682294baba7272cdea543b93274a9116b80
Scala Steward <me@scala-steward.org>
2021-05-20 22:37:26 +0200

    Update wartremover to 2.4.14

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,5 +14,5 @@ object Versions {
   val scalajsCross: String = "1.0.0"
   val scalastyle: String   = "1.0.0"
   val scoverage: String    = "1.6.1"
-  val wartRemover: String  = "2.4.13"
+  val wartRemover: String  = "2.4.14"
 }
