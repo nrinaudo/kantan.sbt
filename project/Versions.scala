4abf759d6f88e1681142df566c9a9864e51cd4a6
Scala Steward <me@scala-steward.org>
2021-07-03 04:52:49 +0200

    Update sbt-scalajs-crossproject to 1.1.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   val sbtSonatype: String  = "3.9.5"
   val sbtUnidoc: String    = "0.4.3"
   val scalajs: String      = "1.3.0"
-  val scalajsCross: String = "1.0.0"
+  val scalajsCross: String = "1.1.0"
   val scalastyle: String   = "1.0.0"
   val scoverage: String    = "1.6.1"
   val wartRemover: String  = "2.4.13"
