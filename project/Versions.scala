3c21c94217117b8502495d70ad27fac9d01d3ff4
Scala Steward <me@scala-steward.org>
2022-04-05 06:43:56 +0200

    Update sbt-scalajs to 1.10.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val sbtSite: String      = "1.4.1"
   val sbtSonatype: String  = "3.9.11"
   val sbtUnidoc: String    = "0.4.3"
-  val scalajs: String      = "1.9.0"
+  val scalajs: String      = "1.10.0"
   val scalajsCross: String = "1.1.0"
   val scalastyle: String   = "1.0.0"
   val scoverage: String    = "1.9.3"
